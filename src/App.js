2adc393883349cf74f04ebdb58df68cb09d1125e
MICHAL <michal.bylica@interia.pl>
2020-04-30 22:06:39 +0200

    Drawing rectangle as two diagonal pixels

--- src/App.js
+++ src/App.js
@@ -6,17 +6,25 @@ import paint_brush_icon_choosen from './assets/paint_brush_icon_choosen.png'
 import rectangle_icon from './assets/rectangle_icon.png'
 import rectangle_icon_choosen from './assets/rectangle_icon_choosen.png'
 
+const canvas = new Array(50).fill('')
+  .map(() => new Array(50).fill(''))
 
 function App() {
-  const [canvas, setCanvas] = useState(
+  /* const [canvas, setCanvas] = useState(
     new Array(50).fill('')
-      .map(() => new Array(50).fill('')))
+      .map(() => new Array(50).fill(''))) */
 
   const [filledPixels, setFilledPixels] = useState([])
+  const [rectanglePixels, setRectanglePixels] = useState([])
   const [color, setColor] = useState('#000000');
   const [paintBrushClicked, paintBrushClickedstate] = useState(false)
   const [rectangleClicked, rectangleClickedstate] = useState(false)
   const [mouseDown, mouseDownState] = useState(false)
+
+  const colorMultiplePixels = (pixels) => {
+    const nextFilledPixels = [...filledPixels, ...pixels]
+    setFilledPixels(nextFilledPixels)
+  }
 
   const handlePixelClick = (rowIndex, pixelIndex) => {
     const isThereNot = (pair) => !(pair[0] === rowIndex && pair[1] === pixelIndex)
@@ -39,17 +47,17 @@ function App() {
       : 'white'
   }
 
-  const isPaintBrushClicked = () => {
+  const setPaintBrushClicked = () => {
     paintBrushClickedstate(!paintBrushClicked)
   }
 
-  const paintBrushChoose = () => {
+  const paintBrushChoose = () => {    //zwraca nazwe image w zalenosci czy wybrany czy nie
     return paintBrushClicked
       ? paint_brush_icon_choosen
       : paint_brush_icon
   }
 
-  const isRectangleClicked = () => {
+  const setRectangleClicked = () => {
     rectangleClickedstate(!rectangleClicked)
   }
 
@@ -59,14 +67,39 @@ function App() {
       : rectangle_icon
   }
 
-  const isMouseDown = () => {
+  const setMouseDown = (event) => {
     mouseDownState(!mouseDown)
-    console.log(mouseDown)
   }
 
-  const isMouseUp = () => {
+  const setMouseUp = () => {
     mouseDownState(!mouseDown)
-    console.log(mouseDown)
+  }
+
+  const rectangleDiagonal = (rowIndex, pixelIndex) => {
+    const nextRectanglePixels = [...rectanglePixels, [rowIndex, pixelIndex, color]]
+    setRectanglePixels(nextRectanglePixels)
+
+    console.log('rectanglePixels:', nextRectanglePixels)
+    if (nextRectanglePixels.length === 2) {
+      rectangleDraw(nextRectanglePixels)
+    }
+  }
+
+  const rectangleDraw = (nextRectanglePixels) => {
+    //debugger
+    colorMultiplePixels([
+      [nextRectanglePixels[0][0], nextRectanglePixels[0][1], nextRectanglePixels[0][2]],
+      [nextRectanglePixels[1][0], nextRectanglePixels[1][1], nextRectanglePixels[1][2]]
+    ])
+    //setFilledPixels([...filledPixels, [rectanglePixels[0][0], rectanglePixels[0][1], color]])
+    console.log('drawn first')
+    console.log(filledPixels)
+    //setFilledPixels([...filledPixels, [rectanglePixels[1][0], rectanglePixels[1][1], color]])
+    console.log('drawn second')
+    console.log(filledPixels)
+
+    setRectanglePixels([])  //czyszczenie tablicy przekątnych
+    console.log('erased')
   }
 
   return (
@@ -77,13 +110,13 @@ function App() {
           <img src={paintBrushChoose()}
             alt="PaintBrushIcon"
             className={'paintBrushIcon'}
-            onClick={() => isPaintBrushClicked()} />
+            onClick={() => setPaintBrushClicked()} />
         </div>
         <div>
           <img src={rectangleChoose()}
             alt="RectangleIcon"
             className={'rectangleIcon'}
-            onClick={() => isRectangleClicked()} />
+            onClick={() => setRectangleClicked()} />
         </div>
         {/* </div> */}
         <div className={"picker-style"}>
@@ -99,9 +132,11 @@ function App() {
               {
                 row.map((pixel, pixelIndex) =>
                   <div
-                    onMouseDown={() => isMouseDown()}
-                    onMouseUp={() => isMouseUp()}
-                    onMouseOver={() => mouseDown && paintBrushClicked && handlePixelClick(rowIndex, pixelIndex)}
+                    //onMouseDown={setMouseDown}    //paintbrush only
+                    onMouseDown={() => rectangleClicked ? rectangleDiagonal(rowIndex, pixelIndex) : setMouseDown()}
+                    //onMouseUp={setMouseUp}        //paintbrush only
+                    onMouseUp={() => rectangleClicked ? rectangleDiagonal(rowIndex, pixelIndex) : setMouseUp()}
+                    onMouseOver={() => (mouseDown && paintBrushClicked && handlePixelClick(rowIndex, pixelIndex))} //dziala
                     key={pixelIndex}
                     onContextMenu={(event) => handleRightClick(event, rowIndex, pixelIndex)}
                     onClick={() => !paintBrushClicked && handlePixelClick(rowIndex, pixelIndex)}
